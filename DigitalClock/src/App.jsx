1219e505071164c156bee5f2873f257bd276fb4c
victor247k <topgspineivictor@gmail.com>
2024-01-19 20:37:34 +0200

    digital clock complete

--- DigitalClock/src/App.jsx
+++ DigitalClock/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import DigitalClock from './DigitalClock'
 
 function App() {
--- DigitalClock/src/DigitalClock.jsx
+++ DigitalClock/src/DigitalClock.jsx
@@ -1,17 +1,30 @@
 import React, {useState, useEffect } from 'react'
 
 function DigitalClock() {
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date())
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const formatTime = () => {
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+    let meridian = hours > 12 ? "PM" : "AM";
+    hours = hours % 12 || 0;
+
+  return `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds} ${meridian}`;
+  }
 
   return (
     <div className="clock-container">
       <p>
-        {hours < 9 ? `0${hours}` : hours}:
-        {minutes < 9 ? `0${minutes}` : minutes}:
-        {seconds < 9 ? `0${seconds}` : seconds}:
-        {true ? " PM" : " AM"}
+        {formatTime()}
       </p>
     </div>
   )
